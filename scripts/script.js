9ba48e3061c14fa2ff4721061e07f75cf8484a25
PetrAdminus <43944943+PetrAdminus@users.noreply.github.com>
2020-07-16 22:30:44 +0500

    lesson23 ready

--- scripts/script.js
+++ scripts/script.js
@@ -84,11 +84,42 @@ window.addEventListener('DOMContentLoaded', () => {
     };
     togglePopup();
 
+    // Табы
+    const tabs = () => {
+        const tabHeader = document.querySelector('.service-header'),
+            tab = tabHeader.querySelectorAll('.service-header-tab'),
+            tabContent = document.querySelectorAll('.service-tab');
+
+        const toggleTabContent = (index) => {
+            for (let i = 0; i < tabContent.length; i++) {
+                if (index === i) {
+                    tab[i].classList.add('active');
+                    tabContent[i].classList.remove('d-none');
+                } else {
+                    tab[i].classList.remove('active');
+                    tabContent[i].classList.add('d-none');
+                }
+            }
+        };
+
+        tabHeader.addEventListener('click', (event) => {
+            let target = event.target.closest('.service-header-tab');
+
+            if (target) {
+                tab.forEach((item, i) => {
+                    if (item === target) {
+                        toggleTabContent(i);
+                    }
+                });
+            }
+        });
+    };
+    tabs();
+
     // Animation
     const animation = () => {
         const popupContent = document.querySelector('.popup-content');
         let count = -40;
-        console.log(count);
 
         popupContent.style.left = '-40%';
 
@@ -99,7 +130,6 @@ window.addEventListener('DOMContentLoaded', () => {
             } else {
                 clearInterval(idInterval);
             }
-            console.log(count);
 
         };
         let idInterval = setInterval(animationPopup, 10);
@@ -110,16 +140,17 @@ window.addEventListener('DOMContentLoaded', () => {
     const toggleImg = () => {
         const commandPhoto = document.querySelectorAll('.command__photo');
 
+        commandPhoto.forEach((elem) => {
+            elem.dataset.about = elem.src;
+        });
+
         commandPhoto.forEach((elem) => elem.addEventListener('mouseover', () => {
             event.target.src = event.target.dataset.img;
         }));
 
-        // commandPhoto.forEach((elem) => elem.addEventListener('mouseout', () => {
-        //     let res = event.target.src.replace(/a/, '');
-
-        //     console.log(res);
-
-        // }));
+        commandPhoto.forEach((elem) => elem.addEventListener('mouseout', () => {
+            event.target.src = event.target.dataset.about;
+        }));
     };
     toggleImg();
 
@@ -128,9 +159,29 @@ window.addEventListener('DOMContentLoaded', () => {
         const calcBlock = document.querySelector('.calc-block'),
             input = calcBlock.querySelectorAll('input');
 
+        const formPhone = document.querySelectorAll('.form-phone');
+
+        const regVal = (target) => {
+            let reg = /\D/g;
+
+            if (reg.test(target.value)) {
+                target.value = target.value.replace(reg, '');
+            }
+        };
+
         input.forEach((elem) => elem.addEventListener('input', () => {
             let target = event.target;
-            let reg = /\D/g;
+            regVal(target);
+        }));
+        formPhone.forEach((elem) => elem.addEventListener('input', () => {
+            let target = event.target;
+            regVal(target);
+        }));
+
+        const formName = document.querySelectorAll('.form-name');
+        formName.forEach((elem) => elem.addEventListener('input', () => {
+            let target = event.target;
+            let reg = /\d/g;
 
             if (reg.test(target.value)) {
                 target.value = target.value.replace(reg, '');
@@ -139,4 +190,107 @@ window.addEventListener('DOMContentLoaded', () => {
 
     };
     validation();
+
+    // Калькулятор
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcCount = document.querySelector('.calc-count'),
+            calcDay = document.querySelector('.calc-day'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            if (target.matches('.calc-type') || target.matches('.calc-square') ||
+                target.matches('.calc-count') || target.matches('.calc-day')) {
+                countSum();
+            }
+        });
+
+    };
+    calc(100);
+
+    // send-ajax-form
+    const sendForm = () => {
+        const errorMessage = 'Что то пошло не так...',
+            loadMessage = 'Загрузка...',
+            successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+
+        const form = document.querySelectorAll('form');
+        const statusMessage = document.createElement('div');
+
+        form.forEach((elem) => {
+            elem.addEventListener('submit', (event) => {
+                event.preventDefault();
+                elem.appendChild(statusMessage);
+                statusMessage.textContent = loadMessage;
+                const formData = new FormData(elem);
+                let body = {};
+
+                for (let val of formData.entries()) {
+                    body[val[0]] = val[1];
+                }
+
+                postData(body, () => {
+                    statusMessage.textContent = successMessage;
+                }, (error) => {
+                    statusMessage.textContent = errorMessage;
+                    console.error(error);
+                });
+
+                elem.reset();
+            });
+        });
+
+        const postData = (body, outputData, errorData) => {
+            const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange', () => {
+
+                if (request.readyState !== 4) {
+                    return;
+
+                }
+
+                if (request.readyState !== 200) {
+                    outputData();
+
+                } else {
+                    errorData(request.status);
+
+                }
+            });
+
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send(JSON.stringify(body));
+        };
+
+    };
+    sendForm();
 });
